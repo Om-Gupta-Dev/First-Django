1386d46e6811a6be0473d520e4aca991a5fe3619
Om <om.gupta.dev@gmail.com>
2020-06-25 18:44:08 +0530

    Trying more new things..

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -3,14 +3,15 @@ import datetime
 
 # Create your views here.
 
+date = datetime.datetime.now()
+dt = {'name':'Om Gupta' , 'date':date }
+
 def index(request):
-    date = datetime.datetime.now()
-    dt = {'name':'Om Gupta' , 'date':date }
     return render(request , 'FirstApp/index.html' , context = dt)
 
 def home(request):
-    return render(request , 'FirstApp/home.html')
+    return render(request , 'FirstApp/home.html' , context = dt)
 
 def contact(request):
-    return render(request , 'FirstApp/contact.html')
-
+    return render(request , 'FirstApp/contact.html' , context = dt)
+    
