c1ed357830c88ae0082eb8132b8ca0f2f4466b05
Om <om.gupta.dev@gmail.com>
2020-07-02 23:59:51 +0530

    Added Password Field..

--- FirstProject/FirstApp/form.py
+++ FirstProject/FirstApp/form.py
@@ -1,25 +1,33 @@
 from django import forms
 from django.core import validators
 
-# def only_gmail(value):
-#     if value[-10:] != "@gmail.com":
-#         raise forms.ValidationError("mail should be Only Gmail..")
+def only_gmail(value):
+    if value[-10:] != "@gmail.com":
+        raise forms.ValidationError("mail should be Only Gmail..")
         
-# def only_alpha(value):
-#     if value.isalpha() != True:
-#         raise forms.ValidationError("Name should Only Contain Alphabets..")
+def only_alpha(value):
+    if value.isalpha() != True:
+        raise forms.ValidationError("Name should Only Contain Alphabets..")
     
 class MessageSend(forms.Form):
-    name = forms.CharField()#validators=[only_alpha , validators.MinLengthValidator(5)])
-    mail = forms.EmailField()#validators = [only_gmail])
+    name = forms.CharField(validators=[only_alpha , validators.MinLengthValidator(5)])
+    mail = forms.EmailField(validators = [only_gmail])
     Message = forms.CharField(widget=forms.Textarea , validators=[validators.MaxLengthValidator(200) , validators.MinLengthValidator(10)])
+    password = forms.CharField(widget = forms.PasswordInput)
+    rpassword = forms.CharField(widget = forms.PasswordInput)
     date = forms.DateField()
         
     def clean(self):
-        inputName = self.cleaned_data['name']
-        inputMail = self.cleaned_data['mail']
-        print(inputName)
-        if inputName.isalpha() == False :
-            raise forms.ValidationError("Name should Only Contain Alphabets")
+        cleaned_data = super().clean()
+        
+        # inputName = cleaned_data['name']
+        inputMail = cleaned_data['mail']
+        inputPwd = cleaned_data['password']
+        inputRPwd = cleaned_data['rpassword']
+        
+        # if inputName.isalpha() == False :
+        #     raise forms.ValidationError("Name should Only Contain Alphabets")
         if inputMail[-10:] != "@gmail.com":
             raise forms.ValidationError("mail should be Only Gmail..")
+        if inputPwd != inputRPwd:
+            raise forms.ValidationError("Password Should Match")
