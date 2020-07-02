08ce750b14abf609c659761dbaeb42a3756e8985
Om <om.gupta.dev@gmail.com>
2020-07-02 14:14:03 +0530

    Modified message validation..

--- FirstProject/FirstApp/form.py
+++ FirstProject/FirstApp/form.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.core import validators
 
 class MessageSend(forms.Form):
     name = forms.CharField()
     mail = forms.EmailField()
-    Message = forms.CharField()
+    Message = forms.CharField(widget=forms.Textarea)
     date = forms.DateField()
     
     def clean_name(self):
@@ -17,7 +18,7 @@ class MessageSend(forms.Form):
     def clean_Message(self):
         inputMessage = self.cleaned_data['Message']
         
-        if 200 < len(inputMessage) < 10:
+        if len(inputMessage) < 10 or 200 < len(inputMessage):
             raise forms.ValidationError("Message length Should be greater than 10 and less than 200..")
         else:
             return inputMessage
