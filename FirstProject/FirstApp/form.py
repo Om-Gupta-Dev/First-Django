f30a4fda3f63a18d15f0c72fbacdc0f55c520ad1
Om <om.gupta.dev@gmail.com>
2020-07-03 19:09:53 +0530

    Made Contact page responsive and saving data in database..

--- FirstProject/FirstApp/form.py
+++ FirstProject/FirstApp/form.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core import validators
+from FirstApp import models
 
 def only_gmail(value):
     print("validating mail in def")
@@ -17,35 +18,18 @@ def bot_handler(value):
         raise forms.ValidationError("Thanks Bot")
     
     
-class MessageSend(forms.Form):
-    name = forms.CharField(validators=[only_alpha , validators.MinLengthValidator(5)])
-    mail = forms.EmailField(validators=[only_gmail])
-    Message = forms.CharField(widget=forms.Textarea , validators=[validators.MaxLengthValidator(200) , validators.MinLengthValidator(10)])
-    password = forms.CharField(label = "Passsword(Again)" , widget = forms.PasswordInput)
-    rpassword = forms.CharField(widget = forms.PasswordInput)
-    date = forms.DateField()
-    bot_handler = forms.CharField(required = False , widget = forms.HiddenInput , validators = [bot_handler])
+class MessageSend(forms.ModelForm):
+    class Meta:
+        model = models.Message
+        fields = '__all__'
+    
+    
+    # name = forms.CharField(validators=[only_alpha , validators.MinLengthValidator(5)])
+    # mail = forms.EmailField(validators=[only_gmail])
+    # Message = forms.CharField(widget=forms.Textarea , validators=[validators.MaxLengthValidator(200) , validators.MinLengthValidator(10)])
+    # password = forms.CharField(label = "Passsword(Again)" , widget = forms.PasswordInput)
+    # rpassword = forms.CharField(widget = forms.PasswordInput)
+    # date = forms.DateField()
+    # bot_handler = forms.CharField(required = False , widget = forms.HiddenInput , validators = [bot_handler])
         
     
-        
-    # def clean(self):
-    #     xx = self.cleaned_data
-    #     print("---------------------------/n/n",xx)
-        
-    #     cleaned_data = super().clean()
-    #     print(cleaned_data)
-        
-    #     inputName = xx['name']
-    #     print(inputName)
-    #     inputMail = xx['mail']
-    #     inputPwd = xx['password']
-    #     inputRPwd = xx['rpassword']
-        
-    #     # if inputName.isalpha() == False :
-    #     #     raise forms.ValidationError("Name should Only Contain Alphabets")
-    #     if inputMail[-10:] != "@gmail.com":
-    #         print("validating mail in Clean")
-    #         raise forms.ValidationError("mail should be Only Gmail..")
-        
-    #     if inputPwd != inputRPwd:
-    #         raise forms.ValidationError("Password Should Match")
--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -49,6 +49,8 @@ def contact(request):
             print("e-Mail : " , forms.cleaned_data['mail'] )
             print("Message : " , forms.cleaned_data['Message'] )
             print("Date : " , forms.cleaned_data['date'] )
+            
+            forms.save(commit=True)
             return render(request , 'FirstApp/thank.html')
         else:
             return render(request , 'FirstApp/contact.html' , context = {'data':data , 'form':forms})
