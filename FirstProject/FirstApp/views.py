db2d955e8ad86fe6bfa686cdbe000776be8c156a
Om <om.gupta.dev@gmail.com>
2020-07-06 13:44:43 +0530

    Started Session using Cookies..

--- FirstProject/FirstApp/views.py
+++ FirstProject/FirstApp/views.py
@@ -10,6 +10,8 @@ dt = {'name':'Om Gupta' , 'date':date }
 
 def index(request):
     date = datetime.datetime.now()
+    count = int(request.COOKIES.get('count' , 0 ))
+    count += 1
     indexPage = {'head1':'First Heading in index Page' , 
                  'para1':'First Paragraph in index Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!' , 'name':'Om Gupta' , 'date':date , 
                  
@@ -17,11 +19,17 @@ def index(request):
                  'para2':'Second Paragraph in index Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!' , 
                  
                  'head3':'Third Heading in index Page' , 
-                 'para3':'Third Paragraph in index Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!'}
-    return render(request , 'FirstApp/index.html' , context = indexPage)
+                 'para3':'Third Paragraph in index Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!' , 
+                 
+                 'count':count}
+    response = render(request , 'FirstApp/index.html' , context = indexPage)
+    response.set_cookie('count' , count )
+    return response
 
 def home(request):
     date = datetime.datetime.now()
+    count = int(request.COOKIES.get('count' , 0 ))
+    count += 1  
     homePage = {'head1':'First Heading in home Page' , 
                 'para1':'First Paragraph in home Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! ' , 
                 
@@ -29,8 +37,12 @@ def home(request):
                 'para2':'Second Paragraph in home Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!' , 
                 
                 'head3':'Third Heading in home Page' , 
-                'para3':'Third Paragraph in home Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!'}
-    return render(request , 'FirstApp/home.html' , context = homePage)
+                'para3':'Third Paragraph in home Page.. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consectetur ab eligendi nemo voluptates inventore provident minus sunt in numquam!' , 
+                
+                'count':count}
+    response = render(request , 'FirstApp/home.html' , context = homePage)
+    response.set_cookie('count' , count )
+    return response
 
 
 def contact(request):
@@ -39,7 +51,11 @@ def contact(request):
     
     if request.method == "GET":
         forms = form.MessageSend()
-        return render(request , 'FirstApp/contact.html' , context = {'data':data , 'form':forms})
+        count = int(request.COOKIES.get('count' , 0 ))
+        count += 1
+        response = render(request , 'FirstApp/contact.html' , context = {'data':data , 'form':forms , 'count':count})
+        response.set_cookie('count' , count )
+        return response
         
     if request.method == "POST":
         forms = form.MessageSend(request.POST)
